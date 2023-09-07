a3199ada594bb991bec2b3137bbae97df5362915
AliTheFox86 <37238268+AliTheFox86@users.noreply.github.com>
2023-09-07 10:02:54 +0800

    update paper link

--- nerf_daily_arxiv.py
+++ nerf_daily_arxiv.py
@@ -346,24 +346,24 @@ def json_to_md(filename,md_filename,
                 top_info = top_info.replace(' ','-').replace('.','')
                 f.write(f"<p align=right>(<a href={top_info.lower()}>back to top</a>)</p>\n\n")
                 
-        # if show_badge == True:
-        #     # we don't like long string, break it!
-        #     f.write((f"[contributors-shield]: https://img.shields.io/github/"
-        #              f"contributors/Vincentqyw/cv-arxiv-daily.svg?style=for-the-badge\n"))
-        #     f.write((f"[contributors-url]: https://github.com/Vincentqyw/"
-        #              f"cv-arxiv-daily/graphs/contributors\n"))
-        #     f.write((f"[forks-shield]: https://img.shields.io/github/forks/Vincentqyw/"
-        #              f"cv-arxiv-daily.svg?style=for-the-badge\n"))
-        #     f.write((f"[forks-url]: https://github.com/Vincentqyw/"
-        #              f"cv-arxiv-daily/network/members\n"))
-        #     f.write((f"[stars-shield]: https://img.shields.io/github/stars/Vincentqyw/"
-        #              f"cv-arxiv-daily.svg?style=for-the-badge\n"))
-        #     f.write((f"[stars-url]: https://github.com/Vincentqyw/"
-        #              f"cv-arxiv-daily/stargazers\n"))
-        #     f.write((f"[issues-shield]: https://img.shields.io/github/issues/Vincentqyw/"
-        #              f"cv-arxiv-daily.svg?style=for-the-badge\n"))
-        #     f.write((f"[issues-url]: https://github.com/Vincentqyw/"
-        #              f"cv-arxiv-daily/issues\n\n"))
+         if show_badge == True:
+             # we don't like long string, break it!
+             f.write((f"[contributors-shield]: https://img.shields.io/github/"
+                      f"contributors/AliTheFox86/nerf-arxiv-daily.svg?style=for-the-badge\n"))
+             f.write((f"[contributors-url]: https://github.com/AliTheFox86/"
+                      f"nerf-arxiv-daily/graphs/contributors\n"))
+            f.write((f"[forks-shield]: https://img.shields.io/github/forks/AliTheFox86/"
+                     f"nerf-arxiv-daily.svg?style=for-the-badge\n"))
+            f.write((f"[forks-url]: https://github.com/AliTheFox86/"
+                     f"nerf-arxiv-daily/network/members\n"))
+            f.write((f"[stars-shield]: https://img.shields.io/github/stars/AliTheFox86/"
+                     f"nerf-arxiv-daily.svg?style=for-the-badge\n"))
+            f.write((f"[stars-url]: https://github.com/AliTheFox86/"
+                     f"nerf-arxiv-daily/stargazers\n"))
+            f.write((f"[issues-shield]: https://img.shields.io/github/issues/AliTheFox86/"
+                     f"nerf-arxiv-daily.svg?style=for-the-badge\n"))
+            f.write((f"[issues-url]: https://github.com/AliTheFox86/"
+                     f"nerf-arxiv-daily/issues\n\n"))
                 
     logging.info(f"{task} finished")        
 
